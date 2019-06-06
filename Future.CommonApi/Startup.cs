00eab05547be0205a1e139fddf1a77ddfe1d3a7e
易林军 <1441457008@qq.com>
2019-06-07 01:01:18 +0800

    add

--- Future.CommonApi/Startup.cs
+++ Future.CommonApi/Startup.cs
@@ -18,7 +18,9 @@ namespace Future.CommonApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -38,7 +40,9 @@ namespace Future.CommonApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -61,7 +65,7 @@ namespace Future.CommonApi
             app.UseSwaggerUI(action =>
             {
                 action.ShowExtensions();
-                action.SwaggerEndpoint("/commonapi/swagger/v1/swagger.json", "V1 Docs");
+                action.SwaggerEndpoint("/common/swagger/v1/swagger.json", "V1 Docs");
                 action.RoutePrefix = string.Empty; ;
             });
         }
